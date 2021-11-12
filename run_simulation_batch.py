56b5f378f297a35b483bae98e8e0f114f2709e04
Ben Jenkins <bdj746@student.bham.ac.uk>
2021-11-12 15:47:21 +0000

    Started implementing automated input file generation using template

--- run_simulation_batch.py
+++ run_simulation_batch.py
@@ -1,5 +1,20 @@
 from liggghts import liggghts
+import numpy as np
+import os
 
-lmp = liggghts()
+## Orifice Sizes
 
-lmp.file('Run_GF_Sim.liggghts') # Read LIGGGHTS file
+orifice_size = [2]  # mm ((TEST))
+# orifice_size = [2, 4, 8, 12, 18, 22, 28]  # mm
+# orifice_size = np.divide(orifice_size, 1000)  # Convert to meters
+
+for i in range(len(orifice_size)):
+
+    print(orifice_size[i])
+    os.popen('cp Run_GF_Sim.liggghts Run_GF_Sim_{}.liggghts'.format(orifice_size[i]))
+
+    with open(os.path.join(""))
+
+# lmp = liggghts()
+
+# lmp.file('Run_GF_Sim.liggghts')  # Read LIGGGHTS file
